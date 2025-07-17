6e5499f5f69a29e8215c475d63f1c4ef3c6a5373
Lucas Ribeiro <lucasaraujodesa@hotmail.com>
2025-07-17 14:46:52 -0300

    feat: implement CSV processing for quiz questions and answers

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 type Question struct {
@@ -30,11 +32,49 @@ func (g *GameState) Init() {
 	}
 
 	g.Name = name[:len(name)-2]
-
+	
 	fmt.Printf("Olá, %s! Vamos começar o quiz.\n", g.Name)
+}
+
+func (g *GameState) ProcessCSV() {
+	file, err := os.Open("quizgo.csv")
+	if err != nil {
+		panic("Erro ao abrir o arquivo CSV")
+	}
+
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		panic("Erro ao ler o arquivo CSV")
+	}
+
+	for index, record := range records {
+		if index > 0 {
+			question := Question {
+				Text: record[0],
+				Answer: toInt(record[len(record)-1]),
+			}
+
+			for i := 1; i < len(record)-1; i++ {
+				question.Options = append(question.Options, record[i])
+			}
+			g.Questions = append(g.Questions, question)
+		}
+	}
 }
 
 func main() {
 	game := &GameState{}
 	game.Init()
+	go game.ProcessCSV()
+}
+
+func toInt(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic("Erro ao converter string para inteiro: " + err.Error())
+	}
+	return i
 }
