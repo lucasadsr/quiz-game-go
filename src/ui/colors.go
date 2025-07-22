2eee0ea3fcf246108177ccebd4573f78bf1daa9b
Lucas Ribeiro <lucasaraujodesa@hotmail.com>
2025-07-22 14:51:55 -0300

    fix: specify string type for color constants in colors.go

--- src/ui/colors.go
+++ src/ui/colors.go
@@ -3,10 +3,10 @@ package ui
 import "fmt"
 
 const (
-	Cyan  = "\033[36m"
-	Reset = "\033[0m"
-	Green = "\033[32m"
-	Red   = "\033[31m"
+	Cyan  string = "\033[36m"
+	Reset string = "\033[0m"
+	Green string = "\033[32m"
+	Red   string = "\033[31m"
 )
 
 func SuccessText(text string) {
